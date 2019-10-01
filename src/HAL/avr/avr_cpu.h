9d431734eccddf6fbd42e0f95a0bcebcf83a0faf
alex <mac@gore.lan>
2019-10-01 13:17:04 +0200

    add avr 328 (only test)

--- src/HAL/avr/avr_cpu.h
+++ src/HAL/avr/avr_cpu.h
@@ -17,23 +17,29 @@ namespace UHAL
 
     template< avr_cpu_type cpu> class GenericCPU< cpu>
     {
-        #if defined( __AVR_ATmega168__)
+#if defined( __AVR_ATmega168__)
             static_assert(cpu == AVR_ATmega168, "CPU does not match compiler settings");
-        #endif
+#endif
 
-        #if defined( __AVR_ATmega328__)
+#if defined( __AVR_ATmega328__)
             static_assert(cpu == AVR_ATmega328, "CPU does not match compiler settings");
-        #endif
+#endif
 
-        #if defined( __AVR_ATmega328P__)
+#if defined( __AVR_ATmega328P__)
             static_assert(cpu == AVR_ATmega328P, "CPU does not match compiler settings");
-        #endif
+            public:
+                static const long ClockMhz = F_CPU;
+                typedef  enum {
+                    PIN_PB0=12, PIN_PB1=13, PIN_PB2=14, PIN_PB3=15, PIN_PB4=16, PIN_PB5=17, PIN_PB6=7,  PIN_PB7=8,
+                    PIN_PC0=23, PIN_PC1=24, PIN_PC2=25, PIN_PC3=26, PIN_PC4=27, PIN_PC5=28, PIN_PC6=29,
+                    PIN_PD0=30, PIN_PD1=31, PIN_PD2=32, PIN_PD3=1,  PIN_PD4=2
+                }cpu_pins;
+#endif
 
-        #if defined( __AVR_ATmega1280__)
+#if defined( __AVR_ATmega1280__)
             static_assert(cpu == AVR_ATmega1280, "CPU does not match compiler settings");
-        #endif
-
-        #if defined( __AVR_ATmega2560__)
+#endif
+#if defined( __AVR_ATmega2560__)
             static_assert(cpu == AVR_ATmega2560, "CPU does not match compiler settings"); 
             public:
                 static const long ClockMhz = F_CPU;
@@ -41,9 +47,8 @@ namespace UHAL
                     PIN_PE0=2, PIN_PE1=3, PIN_PE2=4, PIN_PE3=5, PIN_PE4=6, PIN_PE5=7, PIN_PE6=8, PIN_PE7=9,
                     PIN_PG5=1
                 }cpu_pins;
-    #endif
+#endif
     };
-
 
 }
 #endif // avrcpzu
--- src/HAL/avr/avr_pin.h
+++ src/HAL/avr/avr_pin.h
@@ -28,8 +28,12 @@ namespace UHAL
 template<> void GenericPin<AVR_ATmega2560, GenericCPU<AVR_ATmega2560>::cpu_pins::PIN_PE5>::modeOut(void)     {HAL_AVR_DIR_PIN_OUT( E,5)}
 template<> void GenericPin<AVR_ATmega2560, GenericCPU<AVR_ATmega2560>::cpu_pins::PIN_PE5>::writeHigh(void)   {HAL_AVR_SET_PIN_HIGH(E,5)}
 template<> void GenericPin<AVR_ATmega2560, GenericCPU<AVR_ATmega2560>::cpu_pins::PIN_PE5>::writeLow(void)    {HAL_AVR_SET_PIN_LOW( E,5)}
+#if defined( __AVR_ATmega328P__)
 
-
+template<> void GenericPin<AVR_ATmega328P, GenericCPU<AVR_ATmega328P>::cpu_pins::PIN_PB0>::modeOut(void)     {HAL_AVR_DIR_PIN_OUT( B,0)}
+template<> void GenericPin<AVR_ATmega328P, GenericCPU<AVR_ATmega328P>::cpu_pins::PIN_PB0>::writeHigh(void)   {HAL_AVR_SET_PIN_HIGH(B,0)}
+template<> void GenericPin<AVR_ATmega328P, GenericCPU<AVR_ATmega328P>::cpu_pins::PIN_PB0>::writeLow(void)    {HAL_AVR_SET_PIN_LOW( B,0)}
+#endif
 
 }//namespace UHAL
 #endif //AVR_PIN_H_
