64b555c56adc355b5028ea70dc8fc5c3ddc50145
alex <mac@gore.lan>
2019-10-01 13:24:32 +0200

    template for pin type

--- src/HAL/avr/avr_cpu.h
+++ src/HAL/avr/avr_cpu.h
@@ -15,6 +15,25 @@ namespace UHAL
     AVR_ATmega1280,
     AVR_ATmega2560}avr_cpu_type;
 
+    template<auto cpu> struct pins;
+
+    template<> struct pins<AVR_ATmega2560>
+    {
+        typedef  enum {
+                    PIN_PE0=2, PIN_PE1=3, PIN_PE2=4, PIN_PE3=5, PIN_PE4=6, PIN_PE5=7, PIN_PE6=8, PIN_PE7=9,
+                    PIN_PG5=1
+                }pin_type;
+    };
+
+    template<> struct pins<AVR_ATmega328P>
+    {
+                typedef  enum {
+                    PIN_PB0=12, PIN_PB1=13, PIN_PB2=14, PIN_PB3=15, PIN_PB4=16, PIN_PB5=17, PIN_PB6=7,  PIN_PB7=8,
+                    PIN_PC0=23, PIN_PC1=24, PIN_PC2=25, PIN_PC3=26, PIN_PC4=27, PIN_PC5=28, PIN_PC6=29,
+                    PIN_PD0=30, PIN_PD1=31, PIN_PD2=32, PIN_PD3=1,  PIN_PD4=2
+                }pin_type;
+    };
+
     template< avr_cpu_type cpu> class GenericCPU< cpu>
     {
 #if defined( __AVR_ATmega168__)
@@ -29,11 +48,7 @@ namespace UHAL
             static_assert(cpu == AVR_ATmega328P, "CPU does not match compiler settings");
             public:
                 static const long ClockMhz = F_CPU;
-                typedef  enum {
-                    PIN_PB0=12, PIN_PB1=13, PIN_PB2=14, PIN_PB3=15, PIN_PB4=16, PIN_PB5=17, PIN_PB6=7,  PIN_PB7=8,
-                    PIN_PC0=23, PIN_PC1=24, PIN_PC2=25, PIN_PC3=26, PIN_PC4=27, PIN_PC5=28, PIN_PC6=29,
-                    PIN_PD0=30, PIN_PD1=31, PIN_PD2=32, PIN_PD3=1,  PIN_PD4=2
-                }cpu_pins;
+                typedef pins<AVR_ATmega328P>::pin_type cpu_pins;
 #endif
 
 #if defined( __AVR_ATmega1280__)
@@ -43,10 +58,7 @@ namespace UHAL
             static_assert(cpu == AVR_ATmega2560, "CPU does not match compiler settings"); 
             public:
                 static const long ClockMhz = F_CPU;
-                typedef  enum {
-                    PIN_PE0=2, PIN_PE1=3, PIN_PE2=4, PIN_PE3=5, PIN_PE4=6, PIN_PE5=7, PIN_PE6=8, PIN_PE7=9,
-                    PIN_PG5=1
-                }cpu_pins;
+                typedef pins<AVR_ATmega2560>::pin_type cpu_pins;
 #endif
     };
 
